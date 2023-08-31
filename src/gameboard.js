e3d237eebff66761c31562ea72d6af2fc13cd2d5
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-08-31 18:47:23 -0300

    add method to place ships on board
    Another method, a private one, will check first if all conditions are
    valid before placing the ship.
    Conditions:
    - coordinates must be inside the board
    - the chosen ship must be available
    - the rest of it's spaces must be inside the board too
      - depends on which way the ship is facing
    Once it passes all checks, a new class NamedShip object will be created
    and pushed to the "fleet" and mark the chosen ship on port as already
    used.

--- src/gameboard.js
+++ src/gameboard.js
@@ -14,4 +14,22 @@ export default class Gameboard {
       destroyer: { length: 2, sailed: false },
     };
   }
+
+  placeShip(name, x, y, facing) {
+    const chosenShip = this.port?.[name];
+    if (!chosenShip || chosenShip.sailed) return;
+    if (!this.#isInsideBoard(chosenShip.length, x, y, facing)) return;
+    this.fleet.push(new NamedShip(this.port[name], name, x, y, facing));
+    chosenShip.sailed = true;
+  }
+
+  #isInsideBoard(length, x, y, facing) {
+    if (x < 0 || x > (this.width - 1) || y < 0 || y > (this.height - 1)) return false;
+    if (!['north', 'south', 'east', 'west'].includes(facing)) return false;
+    if (facing === 'north' && y + (length - 1) > (this.height - 1)) return false;
+    if (facing === 'south' && y - (length - 1) < 0) return false;
+    if (facing === 'east' && x - (length - 1) < 0) return false;
+    if (facing === 'west' && x + (length - 1) > (this.width - 1)) return false;
+    return true;
+  }
 }
