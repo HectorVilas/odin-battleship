ae9b15cabf4c533f51349dbb507dccf7b1b78da1
Hector Vilas <96928935+HectorVilas@users.noreply.github.com>
2023-09-13 17:06:55 -0300

    create main DOM elements

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css';
+
+const body = document.querySelector('body');
+const app = document.createElement('main');
+const player1 = document.createElement('div');
+const player2 = document.createElement('div');
+const player1Board = document.createElement('div');
+const player2Board = document.createElement('div');
+
+app.setAttribute('id', 'app');
+[player1, player2].forEach((player, i) => player.setAttribute('id', `player-${i + 1}`));
+[player1Board, player2Board].forEach((board) => board.classList.add('gameboard'));
+
+player1.append(player1Board);
+player2.append(player2Board);
+app.append(player1, player2);
+body.append(app);
